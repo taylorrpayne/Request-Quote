24d9992dd8fc770888e9c54af4d213e36b5c3a9d
piyush15 <piyush@planetx.in>
2018-09-27 10:16:20 +0530

    add from address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { :host => 'requestaquoteapp.com' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
@@ -102,5 +103,5 @@ Rails.application.configure do
     :user_name => 'john.martine1234@gmail.com',
     :password => 'JOHNMartin1234'
   }
-  
+
 end
