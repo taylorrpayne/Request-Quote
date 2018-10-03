cc3df55fd792e9658c30239534a41d69a0c0b381
piyush15 <piyush@planetx.in>
2018-10-03 16:10:45 +0530

    remove extra code

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,13 +1,13 @@
 class OrderMailer < ApplicationMailer
 	def customer_mail(subject,to,first_name)
 		@first_name = first_name
-		mail from: "bill@miataroadster.com <john.martine1234@gmail.com>", to: to,reply_to: "bill@miataroadster.com", subject: subject
+		mail from: "bill@miataroadster.com", to: to,reply_to: "bill@miataroadster.com", subject: subject
 	end
 
 	def admin_mail(subject,to,draft_order,store_name,currency_symbol)
 		@draft_order = draft_order
 		@store_name = store_name
 		@currency_symbol = currency_symbol
-		mail from: "bill@miataroadster.com <john.martine1234@gmail.com>", to: to,reply_to: "bill@miataroadster.com", subject: subject
+		mail from: "bill@miataroadster.com", to: to,reply_to: "bill@miataroadster.com", subject: subject
 	end
 end
